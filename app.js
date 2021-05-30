1ffa556754dba4d56ae4504842f13331f8de9cf0
josh <joshua.c.yap@gmail.com>
2021-05-30 05:28:33 -0500

    quickly throwing basic media queries

---
+++ app.js
@@ -0,0 +1,50 @@
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+  
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function displayModal() {
+    modal.style.display = "block";
+}
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     setTimeout(displayModal, 1000)
+// });
+
+
+
+
+let targetDate = new Date("Dec 12, 2021 08:00:00");
+let x = setInterval(function() {
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = targetDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
